86c58f55405fd71c09436d9d718a12e3d42f7006
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-06-02 12:36:45 +0300

    Switch benchmarks

--- 0_conf.hpp
+++ 0_conf.hpp
@@ -26,26 +26,26 @@
 
 
 ////////////////////////////// Section 1
-#include "algorithm_1.hpp"
-#include "algorithm_2.hpp"
-#include "algorithm_3.hpp"
-#include "algorithm_4.hpp"
-#include "algorithm_5.hpp"
-#include "algorithm_6.hpp"
-#include "algorithm_x.hpp"
+//#include "algorithm_1.hpp"
+//#include "algorithm_2.hpp"
+//#include "algorithm_3.hpp"
+//#include "algorithm_4.hpp"
+//#include "algorithm_5.hpp"
+//#include "algorithm_6.hpp"
+//#include "algorithm_x.hpp"
 
 ////////////////////////////// Section 2
-//#include "container_1.hpp"
-//#include "container_2.hpp"
-//#include "container_3.hpp"
+#include "container_1.hpp"
+#include "container_2.hpp"
+#include "container_3.hpp"
 
 ////////////////////////////// Section 2, vector features
-//#include "container_4.hpp"
-//#include "container_5.hpp"
+#include "container_4.hpp"
+#include "container_5.hpp"
 
 ////////////////////////////// Section 2, associative
-//#include "container_6.hpp"
-//#include "container_7.hpp"
+#include "container_6.hpp"
+#include "container_7.hpp"
 
 
 ////////////////////////////// Section 3, multithreading
