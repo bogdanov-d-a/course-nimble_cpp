7fe58b0b50b56494d20782e26f3fb98138a6ce34
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-06-02 10:36:51 +0300

    Disable unneeded benchmarks

--- 0_conf.hpp
+++ 0_conf.hpp
@@ -35,17 +35,17 @@
 #include "algorithm_x.hpp"
 
 ////////////////////////////// Section 2
-#include "container_1.hpp"
-#include "container_2.hpp"
-#include "container_3.hpp"
+//#include "container_1.hpp"
+//#include "container_2.hpp"
+//#include "container_3.hpp"
 
 ////////////////////////////// Section 2, vector features
-#include "container_4.hpp"
-#include "container_5.hpp"
+//#include "container_4.hpp"
+//#include "container_5.hpp"
 
 ////////////////////////////// Section 2, associative
-#include "container_6.hpp"
-#include "container_7.hpp"
+//#include "container_6.hpp"
+//#include "container_7.hpp"
 
 
 ////////////////////////////// Section 3, multithreading
